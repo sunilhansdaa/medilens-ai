afa38ded30c4a2661f55e47d3fd4f1a2c820f931
cs-sunil <sunilhansda7914@gmail.com>
2026-06-10 07:57:53 +0530

    Convert server to CommonJS

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
-import app from "./app.js";
-import connectDB from "./config/db.js";
+const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
